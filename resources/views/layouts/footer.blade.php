270ebf18954cf471f977e0f8a478328b7b3167f3
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-01-24 00:02:07 +0300

    ..

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,6 +7,7 @@
 <script type="text/javascript" src="/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
 <script type="text/javascript" src="/js/app.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/lodash@4.17.15/lodash.min.js"></script>
 @yield('scripts')
 </body>
 </html>
