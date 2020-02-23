9049c4df5a37de058a3e09e2bdde78e83295b66b
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-02-24 02:41:16 +0300

    wip

--- resources/views/depo_durum.blade.php
+++ resources/views/depo_durum.blade.php
@@ -15,15 +15,15 @@
         <div class="list-group">
             <a href='#' @click='scroll(index)' :class='stil1(index,lkodlar)' v-for='(lkodlar,index) in sonuclar'>
                 <h3>@{{ index }}</h3>
-                <div class="list-group" :ref='index' v-if='alt && alt.length>0'>
-                    <a  @click='scroll(index2); seviye2 = index2' :class='stil2(index2)' class="list-group-item" v-for='(depolar, index2) in alt'>
+                <div class="list-group" :ref='index' v-if='seviye1=index1 && alt && alt.length>0'>
+                    <a  @click='scroll(index2); seviye2 = index2' :class='stil2(index2,depolar)' class="list-group-item" v-for='(depolar, index2) in alt1'>
                         <h4>@{{ index2 }}</h4>
-                        <div class="list-group" :ref='index2' v-if='seviye2=index2 && depolar.length>0'>
-                            <a @click='scroll(index3); seviye3=index3' :class='stil3(index3)' class="list-group-item" v-for='(detaylar, index3) in depolar'>
+                        <div class="list-group" :ref='index2' v-if='seviye2=index2 && alt2 && alt2.length>0'>
+                            <a @click='scroll(index3); seviye3=index3' :class='stil3(index3)' class="list-group-item" v-for='(detaylar, index3) in alt2'>
                                 <h5>@{{ index3 }}</h5>
-                                <div :ref='index3' v-if='seviye3=index3 && detaylar.length>0'>
+                                <div :ref='index3' v-if='seviye3=index3 && alt3 && alt3.length>0'>
                                     <table class="table table-hover">
-                                        <tr v-for='detay in detaylar'>
+                                        <tr v-for='detay in alt3'>
                                             <td>@{{ detay.malkod }}</td>
                                             <td>@{{ detay.malad }}</td>
                                         </tr>
@@ -47,32 +47,36 @@
             seviye1:null,
             seviye2:null,
             seviye3:null,
-            alt:null,
+            alt1:null,
+            alt2:null,
+            alt3:null,
             sonuclar:{!! $sonuclar !!},
         },
         methods:{
-            scroll(aaa,bbb){
+            scroll(aaa){
                 this.seviye1 = aaa;
-                this.alt = bbb;
                 var element = this.$refs[aaa];
                 var top = element.offsetTop;
 
                 window.scrollTo(0, top);
                 console.log(aaa,this.seviye1);
             },
-            stil1(secim){
+            stil1(secim,alt1){
+                this.alt1 = alt1;
                 if(this.seviye1 === secim){
                     return 'list-group-item active';
                 }
                 return 'list-group-item';
             },
-            stil2(secim){
+            stil2(secim,alt2){
+                this.alt2 = alt2;
                 if(this.seviye2 === secim){
                     return 'list-group-item active';
                 }
                 return 'list-group-item';
             },
-            stil3(secim){
+            stil3(secim,alt3){
+                this.alt3 = alt3;
                 if(this.seviye3 === secim){
                     return 'list-group-item active';
                 }
