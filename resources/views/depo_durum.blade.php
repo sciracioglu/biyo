23007d50d7cd49778110f66f9f4eb79bc8717aa9
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-02-24 02:45:42 +0300

    wip

--- resources/views/depo_durum.blade.php
+++ resources/views/depo_durum.blade.php
@@ -13,13 +13,13 @@
 <div class="row" id="app">
     <div class="col-md-12">
         <div class="list-group">
-            <a href='#' @click='scroll(index)' :class='stil1(index,lkodlar)' v-for='(lkodlar,index) in sonuclar'>
+            <a href='#' @click='scroll1(index,lkodlar)' :class='stil1(index)' v-for='(lkodlar,index) in sonuclar'>
                 <h3>@{{ index }}</h3>
                 <div class="list-group" :ref='index' v-if='seviye1=index1 && alt && alt.length>0'>
-                    <a  @click='scroll(index2); seviye2 = index2' :class='stil2(index2,depolar)' class="list-group-item" v-for='(depolar, index2) in alt1'>
+                    <a  @click='scroll2(index2,depolar); seviye2 = index2' :class='stil2(index2)' class="list-group-item" v-for='(depolar, index2) in alt1'>
                         <h4>@{{ index2 }}</h4>
                         <div class="list-group" :ref='index2' v-if='seviye2=index2 && alt2 && alt2.length>0'>
-                            <a @click='scroll(index3); seviye3=index3' :class='stil3(index3)' class="list-group-item" v-for='(detaylar, index3) in alt2'>
+                            <a @click='scroll3(index3,detaylar); seviye3=index3' :class='stil3(index3)' class="list-group-item" v-for='(detaylar, index3) in alt2'>
                                 <h5>@{{ index3 }}</h5>
                                 <div :ref='index3' v-if='seviye3=index3 && alt3 && alt3.length>0'>
                                     <table class="table table-hover">
@@ -53,8 +53,27 @@
             sonuclar:{!! $sonuclar !!},
         },
         methods:{
-            scroll(aaa){
-                this.seviye1 = aaa;
+            scroll1(aaa,bbb){
+                this.seviye1 = secim;
+                this.alt1 = bbb;
+                var element = this.$refs[aaa];
+                var top = element.offsetTop;
+
+                window.scrollTo(0, top);
+                console.log(aaa,this.seviye1);
+            },
+            scroll2(aaa,bbb){
+                this.seviye2 = secim;
+                this.alt2 = bbb;
+                var element = this.$refs[aaa];
+                var top = element.offsetTop;
+
+                window.scrollTo(0, top);
+                console.log(aaa,this.seviye1);
+            },
+            scroll3(aaa,bbb){
+                this.seviye3 = secim;
+                this.alt3 = bbb;
                 var element = this.$refs[aaa];
                 var top = element.offsetTop;
 
@@ -62,6 +81,7 @@
                 console.log(aaa,this.seviye1);
             },
             stil1(secim,alt1){
+               
                 this.alt1 = alt1;
                 if(this.seviye1 === secim){
                     return 'list-group-item active';
