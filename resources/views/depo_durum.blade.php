c375c4ec0e5678b0ca53f343b52839259a93923f
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-02-24 02:03:38 +0300

    wip

--- resources/views/depo_durum.blade.php
+++ resources/views/depo_durum.blade.php
@@ -13,18 +13,18 @@
 <div class="row" id="app">
     <div class="col-md-12">
         <div class="list-group">
-            <a :href="#index" @click='seviye1=index' :class='stil1(index)' v-for='(lkodlar,index) in sonuclar'>
+            <a @click='scroll(index)' :class='stil1(index)' v-for='(lkodlar,index) in sonuclar'>
                 @{{ index }}
             </a>
-            <div class="list-group" v-if="kontrol(index)">
-                <a :href='#index2' @click='seviye2=index2' :class='stil2(index2)' class="list-group-item" v-for='(depolar, index2) in lkodlar'>
+            <div class="list-group" :ref='index'>
+                <a  @click='scroll(index2)' :class='stil2(index2)' class="list-group-item" v-for='(depolar, index2) in lkodlar'>
                     @{{ index2 }}
                 </a>
-                <div class="list-group" v-if='seviye2==index2'>
-                    <a href='#' @click='seviye3=index3' :class='stil3(index3)' class="list-group-item" v-for='(detaylar, index3) in depolar'>
+                <div class="list-group" :ref='index2'>
+                    <a @click='scroll(index3)' :class='stil3(index3)' class="list-group-item" v-for='(detaylar, index3) in depolar'>
                         @{{ index3 }}
                     </a>
-                    <div v-if='seviye3==index3'>
+                    <div :ref='index3'>
                         <table class="table table-hover">
                             <tr v-for='detay in detaylar'>
                                 <td>@{{ detay.malkod }}</td>
@@ -51,7 +51,11 @@
             sonuclar:{!! $sonuclar !!},
         },
         methods:{
-            kontrol(index){
+            scroll(index){
+                var element = this.$refs[refName];
+                var top = element.offsetTop;
+
+                window.scrollTo(0, top);
                 console.log(index,this.seviye1);
             },
             stil1(secim){
