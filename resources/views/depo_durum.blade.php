076351abc1088e268e542e1275bb01ab4a5cc9f6
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-02-24 04:22:18 +0300

    ..

--- resources/views/depo_durum.blade.php
+++ resources/views/depo_durum.blade.php
@@ -13,62 +13,61 @@
 <div class="row" id="app">
     <div class="col-md-12">
         <div class="spinner-border text-primary" role="status" v-if='!sonuclar'>
-            <span class="sr-only">Loading...</span>
-          </div>
+        </div>
         <div class="accordion" id="accordionExample" v-else>
             <div class="card" v-for='(kodlar,index) in sonuclar'>
-              <div class="card-header" :id="head(index)">
-                <h2 class="mb-0">
-                  <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="hedef(index)" aria-expanded="true" :aria-controls="slugify(index)">
-                    @{{ index }}
-                  </button>
-                </h2>
-              </div>
-              <div :id="slugify(index)" class="collapse" :aria-labelledby="head(index)" data-parent="#accordionExample">
-                <div class="card-body">
-                  <div class="accordion" :id="aci(index,'b')">
-                    <div class="card" v-for='(depolar,index2) in kodlar'>
-                        <div class="card-header" :id="head(index2)">
-                            <h2 class="mb-0">
-                                <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="hedef(index2)" aria-expanded="true" :aria-controls="slugify(index2)">
-                                @{{ index2 }}
-                                </button>
-                            </h2>
-                        </div>
-                        <div :id="slugify(index2)" class="collapse" :aria-labelledby="head(index2)" :data-parent="acc(index,'b')">
-                            <div class="card-body">
-                                <div class="accordion" :id="aci(index2,'c')">
-                                    <div class="card" v-for='(detaylar,index3) in depolar'>
-                                        <div class="card-header" :id="head(index3)">
-                                        <h2 class="mb-0">
-                                            <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="hedef(index3)" aria-expanded="true" :aria-controls="slugify(index3)">
-                                            @{{ index3 }}
-                                            </button>
-                                        </h2>
-                                        </div>
-                                        <div :id="slugify(index3)" class="collapse" :aria-labelledby="head(index3)" :data-parent="acc(index2,'c')">
-                                        <div class="card-body">
-                                            <table class="table table-hover">
-                                                <tr v-for='detay in detaylar'>
-                                                    <td>@{{ detay.malkod }}</td>
-                                                    <td>@{{ detay.malad }}</td>
-                                                    <td>@{{ detay.ozelkod }}</td>
-                                                    <td>@{{ detay.devir }}</td>
-                                                    <td>@{{ detay.cikis }}</td>
-                                                    <td>@{{ detay.miktar }}</td>
-                                                    <td>@{{ detay.seri }}</td>
-                                                </tr>
-                                            </table>
-                                        </div>
+                <div class="card-header" :id="head(index)">
+                    <h2 class="mb-0">
+                    <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="hedef(index)" aria-expanded="true" :aria-controls="slugify(index)">
+                        @{{ index }}
+                    </button>
+                    </h2>
+                </div>
+                <div :id="slugify(index)" class="collapse" :aria-labelledby="head(index)" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <div class="accordion" :id="aci(index,'b')">
+                            <div class="card" v-for='(depolar,index2) in kodlar'>
+                                <div class="card-header" :id="head(index2)">
+                                    <h2 class="mb-0">
+                                        <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="hedef(index2)" aria-expanded="true" :aria-controls="slugify(index2)">
+                                        @{{ index2 }}
+                                        </button>
+                                    </h2>
+                                </div>
+                                <div :id="slugify(index2)" class="collapse" :aria-labelledby="head(index2)" :data-parent="acc(index,'b')">
+                                    <div class="card-body">
+                                        <div class="accordion" :id="aci(index2,'c')">
+                                            <div class="card" v-for='(detaylar,index3) in depolar'>
+                                                <div class="card-header" :id="head(index3)">
+                                                    <h2 class="mb-0">
+                                                        <button class="btn btn-link" type="button" data-toggle="collapse" :data-target="hedef(index3)" aria-expanded="true" :aria-controls="slugify(index3)">
+                                                        @{{ index3 }}
+                                                        </button>
+                                                    </h2>
+                                                </div>
+                                                <div :id="slugify(index3)" class="collapse" :aria-labelledby="head(index3)" :data-parent="acc(index2,'c')">
+                                                    <div class="card-body">
+                                                        <table class="table table-hover">
+                                                            <tr v-for='detay in detaylar'>
+                                                                <td>@{{ detay.malkod }}</td>
+                                                                <td>@{{ detay.malad }}</td>
+                                                                <td>@{{ detay.ozelkod }}</td>
+                                                                <td>@{{ detay.devir }}</td>
+                                                                <td>@{{ detay.cikis }}</td>
+                                                                <td>@{{ detay.miktar }}</td>
+                                                                <td>@{{ detay.seri }}</td>
+                                                            </tr>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                  </div>
                 </div>
-              </div>
             </div>
         </div>
     </div>
@@ -80,13 +79,6 @@
     var vue = new Vue({
         el:'#app',
         data:{
-            ndx:null,
-            seviye1:null,
-            seviye2:null,
-            seviye3:null,
-            alt1:null,
-            alt2:null,
-            alt3:null,
             sonuclar:{!! $sonuclar !!},
         },
         methods:{
@@ -119,50 +111,6 @@
             },
             hedef(i){
                 return '#'+this.slugify(i);
-            },
-            scroll1(aaa){
-                this.seviye1 = aaa;
-                this.alt1 = this.sonuclar[aaa];
-            },
-            scroll2(aaa,bbb){
-                this.seviye2 = aaa;
-                this.alt2 = bbb;
-                var element = this.$refs[aaa];
-                var top = element.offsetTop;
-
-                window.scrollTo(0, top);
-                console.log(aaa,this.seviye1);
-            },
-            scroll3(aaa,bbb){
-                this.seviye3 = aaa;
-                this.alt3 = bbb;
-                var element = this.$refs[aaa];
-                var top = element.offsetTop;
-
-                window.scrollTo(0, top);
-                console.log(aaa,this.seviye1);
-            },
-            stil1(secim,alt1){
-               
-                this.alt1 = alt1;
-                if(this.seviye1 === secim){
-                    return 'list-group-item active';
-                }
-                return 'list-group-item';
-            },
-            stil2(secim,alt2){
-                this.alt2 = alt2;
-                if(this.seviye2 === secim){
-                    return 'list-group-item active';
-                }
-                return 'list-group-item';
-            },
-            stil3(secim,alt3){
-                this.alt3 = alt3;
-                if(this.seviye3 === secim){
-                    return 'list-group-item active';
-                }
-                return 'list-group-item';
             },
         }
     })
