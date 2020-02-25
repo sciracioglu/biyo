a09f633a24f39f8d03264731c89b1ceee35fe9dc
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-02-25 22:58:22 +0300

    ..

--- app/Http/Controllers/DepoDurumController.php
+++ app/Http/Controllers/DepoDurumController.php
@@ -9,39 +9,14 @@ class DepoDurumController extends Controller
     public function index()
     {
         $durumlar = StokDurum::orderBy('STKKRT_ACIKLAMA3')
-                                ->whereNotNull('STKKRT_ACIKLAMA3')
-                                ->where('STKKRT_ACIKLAMA3', '<>', '')
-                                ->where('STKKRT_ACIKLAMA3', 'ACCESSORIES')
-                                ->get()
-                                ->map(function ($durum) {
-                                    return $aciklama[$durum->STKKRT_ACIKLAMA3]=[
-                                        'aciklama' => $durum->STKKRT_ACIKLAMA3,
-                                        'veri'     => [
-                                            'aciklama' => $durum->STKKRT_LKOD8,
-                                            'veri'     => [
-                                                'aciklama' => $durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD,
-                                                'veri'     => [
-                                                    'malad'   => $durum->STKKRT_MALAD,
-                                                    'malkod'  => $durum->MALKOD,
-                                                    'ozelkod' => $durum->STKKRT_OZELKOD,
-                                                    'devir'   => $durum->STOKDEVIR,
-                                                    'cikis'   => $durum->STOKCIKIS,
-                                                    'miktar'  => $durum->STOKMIKTAR,
-                                                    'seri'    => $durum->SERINO
-                                                ]
-                                            ]
-                                        ]
-                                    ];
-                                });
-        dump($durumlar);
-        $durumlar = StokDurum::orderBy('STKKRT_ACIKLAMA3')
                             ->whereNotNull('STKKRT_ACIKLAMA3')
                             ->where('STKKRT_ACIKLAMA3', '<>', '')
                             ->where('STKKRT_ACIKLAMA3', 'ACCESSORIES')
                             ->get();
         $sonuclar = [];
         foreach ($durumlar as $durum) {
-            $sonuclar[$durum->STKKRT_ACIKLAMA3][$durum->STKKRT_LKOD8][$durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD][] =
+            array_push($sonuclar, ['aciklama'=>$durum->STKKRT_ACIKLAMA3, 'lkod8'=>$durum->STKKRT_LKOD8, 'depo'=>$durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD]);
+            $sonuclar[] =
                                                             [
                                                                 'malad'   => $durum->STKKRT_MALAD,
                                                                 'malkod'  => $durum->MALKOD,
