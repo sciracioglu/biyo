765a69426a1d6cd664d6d2b7108b75026551bcaf
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-02-25 23:01:25 +0300

    ..

--- app/Http/Controllers/DepoDurumController.php
+++ app/Http/Controllers/DepoDurumController.php
@@ -15,20 +15,22 @@ class DepoDurumController extends Controller
                             ->get();
         $sonuclar = [];
         foreach ($durumlar as $durum) {
-            array_push($sonuclar, ['aciklama'=>$durum->STKKRT_ACIKLAMA3, 'lkod8'=>$durum->STKKRT_LKOD8, 'depo'=>$durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD]);
+            array_push($sonuclar, ['aciklama'  => $durum->STKKRT_ACIKLAMA3,
+                                    ['lkod8'    => $durum->STKKRT_LKOD8,
+                                    ['depo'     => $durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD]]]);
             $sonuclar[] =
-                                                            [
-                                                                'malad'   => $durum->STKKRT_MALAD,
-                                                                'malkod'  => $durum->MALKOD,
-                                                                'ozelkod' => $durum->STKKRT_OZELKOD,
-                                                                'devir'   => $durum->STOKDEVIR,
-                                                                'cikis'   => $durum->STOKCIKIS,
-                                                                'miktar'  => $durum->STOKMIKTAR,
-                                                                'seri'    => $durum->SERINO
-                                                            ];
+                            [
+                                'malad'   => $durum->STKKRT_MALAD,
+                                'malkod'  => $durum->MALKOD,
+                                'ozelkod' => $durum->STKKRT_OZELKOD,
+                                'devir'   => $durum->STOKDEVIR,
+                                'cikis'   => $durum->STOKCIKIS,
+                                'miktar'  => $durum->STOKMIKTAR,
+                                'seri'    => $durum->SERINO
+                            ];
         }
         $sonuclar = json_encode($sonuclar, JSON_UNESCAPED_UNICODE);
-        dd($sonuclar);
+
         return view('depo_durum', compact('sonuclar'));
     }
 }
