5340ea3c505c204b1ea9c62fbed68f509d8ffbb2
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-03-09 21:01:10 +0300

    ..

--- app/Http/Controllers/DepoDurumController.php
+++ app/Http/Controllers/DepoDurumController.php
@@ -42,7 +42,7 @@ class DepoDurumController extends Controller
             $sonuclar = [];
 
             foreach ($durumlar as $durum) {
-                $sonuclar[$durum->STKKRT_ACIKLAMA3][$durum->STKKRT_ACIKLAMA3 . '_' . $durum->STKKRT_LKOD8][$durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD][] =
+                $sonuclar[$durum->STKKRT_ACIKLAMA3][$durum->STKKRT_LKOD8][$durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD][] =
                                                             [
                                                                 'malad'   => $durum->STKKRT_MALAD,
                                                                 'malkod'  => $durum->MALKOD,
