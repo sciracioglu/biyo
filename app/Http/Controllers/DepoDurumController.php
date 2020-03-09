68acc763d7473908824eb36ba2213c5577344c27
Serkan Ciracioglu <serkan@tedankara.k12.tr>
2020-03-09 20:35:47 +0300

    ..

--- app/Http/Controllers/DepoDurumController.php
+++ app/Http/Controllers/DepoDurumController.php
@@ -40,7 +40,7 @@ class DepoDurumController extends Controller
                                         DB::raw('sum(STOKMIKTAR) as total')
                                     ]);
             $sonuclar = [];
-            dd($durumlar);
+            dd($durumlar->first());
             foreach ($durumlar as $durum) {
                 $sonuclar[$durum->STKKRT_ACIKLAMA3][$durum->STKKRT_LKOD8][$durum->STKKRT_LKOD8 . ' - ' . $durum->DEPOKOD . ' - ' . $durum->DEPOAD][] =
                                                             [
