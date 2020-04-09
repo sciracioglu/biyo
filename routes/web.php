bb6193de3acbff0085f0efce44872d7722677c2d
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-09 23:45:23 +0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,14 @@ Route::post('login', 'LoginController@store')->middleware('throttle:60,5');
 Route::group(['middleware' => ['login']], function () {
     Route::get('/', 'MusteriController@index');
     Route::post('/', 'MusteriController@store');
-
     Route::resource('siparis', 'SiparisController');
-
     Route::get('siparis_liste', 'SiparisListesiController@index');
     Route::get('siparis_liste/{evraksn}', 'SiparisListesiController@show');
     Route::delete('siparis_liste/{id}', 'SiparisListesiController@destroy');
-
     Route::get('depo_durum', 'DepoDurumController@index');
-
     Route::get('satis_rapor', 'SatisRaporController@index');
     // Route::get('siparisler', 'SiparisListesiController@show');
-
     Route::get('stok_durum', 'StokDurumController@index');
-
     Route::get('rapor', 'StokRaporController@index');
-
     Route::delete('logout/{username}', 'LoginController@destroy');
 });
