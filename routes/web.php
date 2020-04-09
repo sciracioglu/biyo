c887745f633f9675e1b90ad2ba999ec7103862c8
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-09 23:39:42 +0300

    ..

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ Route::group(['middleware' => ['login']], function () {
     Route::resource('siparis', 'SiparisController');
 
     Route::get('siparis_liste', 'SiparisListesiController@index');
-    Route::get('siparis_liste/{evraksn}',    Route::delete('siparis_liste/{id}', 'SiparisListesiController@destroy');
+    Route::get('siparis_liste/{evraksn}', 'SiparisListesiController@show');
+    Route::delete('siparis_liste/{id}', 'SiparisListesiController@destroy');
 
     Route::get('depo_durum', 'DepoDurumController@index');
 
