fb6149ec9916cdffbf603f595aaf5e5a2106352d
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-10 00:38:16 +0300

    ..

--- app/Http/Controllers/SatisRaporController.php
+++ app/Http/Controllers/SatisRaporController.php
@@ -8,7 +8,15 @@ class SatisRaporController extends Controller
 {
     public function index()
     {
-        $satis_durumu = DB::select('SELECT * FROM [dbo].[VW_ARG_WEB_SATIS_RAPOR]');
-        dd($satis_durumu);
+        $yillik_satislar = collect(DB::select('SELECT EVRAKYIL,
+        SUM(MIKTAR) AS T_MIKTAR,
+        SUM(EVRAKTUTAR) AS T_TUTAR,
+        SUM(ISKONTO) AS T_ISKONTO,
+        SUM(EVRAKNETTUTAR) AS T_NETTUTAR,
+        SUM(KDV) AS T_KDV,
+        SUM(TOPLAM) AS T_TOPLAM
+         FROM [dbo].[VW_ARG_WEB_SATIS_RAPOR]'));
+
+        return view('satis_rapor', compact('yillik_satislar'));
     }
 }
