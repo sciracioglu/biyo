4cd0d7a229da465db352bfff3c6f07554a415b84
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-09 22:39:52 +0300

    ..

--- resources/views/depo_durum.blade.php
+++ resources/views/depo_durum.blade.php
@@ -94,7 +94,7 @@
             toplam_3:function(ara,ara2,ara3) {
                var strArray2 = ara2.split(" - ");
                var strArray = ara3.split(" - ");
-               var sonuc3 =  _.find(this.uc,{'STKKRT_ACIKLAMA3': ara,'STKKRT_ACIKLAMA3':strArray2[0],'STKKRT_LKOD8':strArray2[1],'DEPOKOD':strArray[1],'DEPOAD':strArray[2]});
+               var sonuc3 =  _.find(this.uc,{'STKKRT_ACIKLAMA3': ara,'STKKRT_ACIKLAMA3':strArray2[0],'STKKRT_LKOD8':strArray2[1],'DEPOKOD':strArray[0],'DEPOAD':strArray[1],'MALKOD':strArray[2]});
                 return sonuc3.total;
             },
             slugify(text) {
