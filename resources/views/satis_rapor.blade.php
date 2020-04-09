16c5fc994dec45fc256c09fd97b706019ad636f1
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-10 00:58:07 +0300

    ..

--- resources/views/satis_rapor.blade.php
+++ resources/views/satis_rapor.blade.php
@@ -12,24 +12,36 @@
 <div class="row" id='app'>
     <div class="col-md-12">
         <div v-if='isLoading'><i class="fa fa-gear faa-spin animated fa-3x"></i></div>
-        <div class="list-group">
-            <div class="list-group-item" v-for='(yil, index) in yillik_satislar'>
-                <div class="row">
-                    <div class="col-md-2">@{{ yil.EVRAKYIL}}</div>
-                    <div class="col-md-1">@{{ yil.T_MIKTAR }}</div>
-                    <div class="col-md-2">@{{ yil.T_TUTAR }}</div>
-                    <div class="col-md-1">@{{ yil.T_ISKONTO }}</div>
-                    <div class="col-md-2">@{{ yil.T_NETTUTAR }}</div>
-                    <div class="col-md-2">@{{ yil.T_KDV }}</div>
-                    <div class="col-md-2">@{{ yil.T_TOPLAM }}</div>
-                </div>
-            </div>
-        </div>
+        <table class="table table-hover table-condenced table-striped">
+            <thead>
+                <tr>
+                    <th>Evrak Yıl</th>
+                    <th>Miktar</th>
+                    <th>Tutar</th>
+                    <th>İskonto</th>
+                    <th>Net Tutar</th>
+                    <th>KDV</th>
+                    <th>Toplam</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for='(yil, index) in yillik_satislar'>
+                    <td>@{{ yil.EVRAKYIL}}</td>
+                    <td class="text-right" v-text='format(yil.T_MIKTAR)'></td>
+                    <td class="text-right" v-text='format(yil.T_TUTAR)'></td>
+                    <td class="text-right" v-text='format(yil.T_ISKONTO)'></td>
+                    <td class="text-right" v-text='format(yil.T_NETTUTAR)'></td>
+                    <td class="text-right" v-text='format(yil.T_KDV)'></td>
+                    <td class="text-right" v-text='format(yil.T_TOPLAM)'></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 @endsection
 
 @section('scripts')
+<script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
 <script>
     var vue = new Vue({
         el:'#app',
@@ -38,6 +50,11 @@
             yillik_satislar:{!! $yillik_satislar !!},
             aylik_satislar:null,
             cari_satislari:null,
+        },
+        computed:{
+            format(rakam){
+                return numeral(rakam) . format('0,0.00');
+            }
         },
         methods:{
 
