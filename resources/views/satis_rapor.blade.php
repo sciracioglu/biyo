9715cdad63db5dda4f4ba0121312b811a587c71d
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-10 17:05:11 +0300

    ..

--- resources/views/satis_rapor.blade.php
+++ resources/views/satis_rapor.blade.php
@@ -39,7 +39,7 @@
                         <td class="text-right" v-text='format(yil.kdv)'></td>
                         <td class="text-right" v-text='format(yil.toplam)'></td>
                     </tr>
-                    <template  v-if='aylik_satislar.length >0 && yil_index = yil.yil' v-for='ay in aylik_satislar'>
+                    <template  v-if='aylik_satislar.length >0 && yil_index == yil.yil' v-for='ay in aylik_satislar'>
                         <tr>
                             <td>@{{ ay.yil}} - @{{ ay.ay }} </td>
                             <td class="text-right" v-text='format(ay.miktar)'></td>
