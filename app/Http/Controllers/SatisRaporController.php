ad887e056a98f00a1de0f3efc26f1c02e76be6a6
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-10 16:20:34 +0300

    satis raporu

--- app/Http/Controllers/SatisRaporController.php
+++ app/Http/Controllers/SatisRaporController.php
@@ -17,8 +17,117 @@ class SatisRaporController extends Controller
         SUM(TOPLAM) AS T_TOPLAM
          FROM [dbo].[VW_ARG_WEB_SATIS_RAPOR]
          GROUP BY EVRAKYIL
-         ORDER BY EVRAKYIL DESC'));
+         ORDER BY EVRAKYIL DESC'))
+         ->map(function($yil){
+             return [
+                 'yil' => $yil->EVRAKYIL,
+                 'miktar' => $yil->T_MIKTAR,
+                 'tutar' => $yil->T_TUTUAR,
+                 'iskonto' => $yil->T_ISKONTO,
+                 'nettutar' => $yil->T_NETTUTAR,
+                 'kdv' => $yil->T_KDV,
+                 'toplam' => $yil->T_TOPLAM,
+                 'aylar' => $this->aylar($yil->EVRAKYIL),
+             ];
+         });
 
         return view('satis_rapor', compact('yillik_satislar'));
+    }
+
+    private function aylar($yil)
+    {
+        return collect(DB::select('SELECT EVRAKYIL, EVRAKAY,
+        SUM(MIKTAR) AS T_MIKTAR,
+        SUM(EVRAKTUTAR) AS T_TUTAR,
+        SUM(ISKONTO) AS T_ISKONTO,
+        SUM(EVRAKNETTUTAR) AS T_NETTUTAR,
+        SUM(KDV) AS T_KDV,
+        SUM(TOPLAM) AS T_TOPLAM
+         FROM [dbo].[VW_ARG_WEB_SATIS_RAPOR]
+         WHERE EVRAKYIL = ?
+         GROUP BY EVRAKYIL, EVRAKAY
+         ORDER BY EVRAKYIL DESC, EVRAKAY ASC',[$yil]))
+         ->map(function($ay){
+            return [
+                'yil' => $ay->EVRAKYIL,
+                'ay' => $ay->EVRAKAY,
+                'miktar' => $ay->T_MIKTAR,
+                'tutar' => $ay->T_TUTUAR,
+                'iskonto' => $ay->T_ISKONTO,
+                'nettutar' => $ay->T_NETTUTAR,
+                'kdv' => $ay->T_KDV,
+                'toplam' => $ay->T_TOPLAM,
+                'musteriler' => $this->musteriler($ay->EVRAKYIL, $ay->EVARAKAY),
+            ];
+         });
+    }
+
+    private function musteriler($yil, $ay)
+    {
+        return collect(DB::select('SELECT EVRAKYIL, EVRAKAY, CARKRT_UNVAN, CARKRT_UNVAN2, HESAPKOD
+        SUM(MIKTAR) AS T_MIKTAR,
+        SUM(EVRAKTUTAR) AS T_TUTAR,
+        SUM(ISKONTO) AS T_ISKONTO,
+        SUM(EVRAKNETTUTAR) AS T_NETTUTAR,
+        SUM(KDV) AS T_KDV,
+        SUM(TOPLAM) AS T_TOPLAM
+         FROM [dbo].[VW_ARG_WEB_SATIS_RAPOR]
+         WHERE EVRAKYIL = ?
+         AND EVRAKAY = ?
+         GROUP BY EVRAKYIL, EVRAKAY, HESAPKOD, CARKRT_UNVAN, CARKRT_UNVAN2
+         ORDER BY EVRAKTARIH DESC',[$yil, $ay]))
+         ->map(function($musteri){
+            return [
+                'yil' => $musteri->EVRAKYIL,
+                'ay' => $musteri->EVRAKAY,
+                'unvan' => $musteri->CARKRT_UNVAN.' '.$musteri->CARKRT_UNVAN2,
+                'miktar' => $musteri->T_MIKTAR,
+                'tutar' => $musteri->T_TUTUAR,
+                'iskonto' => $musteri->T_ISKONTO,
+                'nettutar' => $musteri->T_NETTUTAR,
+                'kdv' => $musteri->T_KDV,
+                'toplam' => $musteri->T_TOPLAM,
+                'hesaplar' => $this->hesaplar($musteri->EVRAKYIL, $musteri->EVARAKAY, $musteri->HESAPKOD),
+            ];
+         });
+    }
+
+    private function hesaplar($yil, $ay, $hesapkod)
+    {
+        return collect(DB::select('SELECT *
+         FROM [dbo].[VW_ARG_WEB_SATIS_RAPOR]
+         WHERE EVRAKYIL = ?
+         AND EVRAKAY = ?
+         AND HESAPKOD = ?
+         ORDER BY EVRAKYIL DESC',[$yil, $ay, $hesapkod]))
+         ->map(function($hesap){
+            return [
+                'yil' => $hesap->EVRAKYIL,
+                'ay' => $hesap->EVRAKAY,
+                'unvan' => $hesap->CARKRT_UNVAN . ' '. $hesap->CARKRT_UNVAN2,
+                'evrak_net_tutar' => $hesap->EVRAKNETTUTAR,
+                'net_tutar' => $hesap->NETTUTAR,
+                'toplam' => $hesap->TOPLAM,
+                'birim_fiyat' => $hesap->BIRIMFIYAT,
+                'evrak_no' => $hesap->EVRAKNO,
+                'evrak_tarih' => $hesap->EVRAKTARIH,
+                'evrak_tip' => $hesap->EVRAKTIP,
+                'evrak_tutar' => $hesap->EVRAKTUTAR,
+                'hesapkod' => $hesap->HESAPKOD,
+                'iskonto' => $hesap->ISKONTO,
+                'islem_tip' => $hesap->ISLEMTIP,
+                'kdv' => $hesap->KDV,
+                'malkod' => $hesap->MALKOD,
+                'miktar' => $hesap->MIKTAR,
+                'serino' => $hesap->SERINO,
+                'tutar' => $hesap->TUTAR,
+                'vade_tarih' => $hesap->VADETARIH,
+                'aciklama' => $hesap->STKKRT_ACIKLAMA1,
+                'grupkod' => $hesap->STKKRT_GRUPKOD,
+                'malad' => $hesap->STKKRT_MALAD,
+                'malkod' => $hesap->STKKRT_MKOD1,
+                'tipkod' => $hesap->STKKRT_TIPKOD,
+            ];
+         });
     }
 }
--- resources/views/satis_rapor.blade.php
+++ resources/views/satis_rapor.blade.php
@@ -25,15 +25,83 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for='(yil, index) in yillik_satislar'>
-                    <td>@{{ yil.EVRAKYIL}}</td>
-                    <td class="text-right" v-text='format(yil.T_MIKTAR)'></td>
-                    <td class="text-right" v-text='format(yil.T_TUTAR)'></td>
-                    <td class="text-right" v-text='format(yil.T_ISKONTO)'></td>
-                    <td class="text-right" v-text='format(yil.T_NETTUTAR)'></td>
-                    <td class="text-right" v-text='format(yil.T_KDV)'></td>
-                    <td class="text-right" v-text='format(yil.T_TOPLAM)'></td>
-                </tr>
+                <div  v-for='(yil, index) in yillik_satislar'>
+                    <tr>
+                        <td>
+                            <button type="button" class="btn btn-xs btn-default icon-only" @click='yilAc(yil.yil)'>
+                                <i class="fa fa-plus"></i>
+                            </button>
+                            @{{ yil.yil}}</td>
+                        <td class="text-right" v-text='format(yil.miktar)'></td>
+                        <td class="text-right" v-text='format(yil.tutar)'></td>
+                        <td class="text-right" v-text='format(yil.iskonto)'></td>
+                        <td class="text-right" v-text='format(yil.nettutar)'></td>
+                        <td class="text-right" v-text='format(yil.kdv)'></td>
+                        <td class="text-right" v-text='format(yil.toplam)'></td>
+                    </tr>
+                    <div  v-if='yil.aylar.length >0 && yil_index = yil.yil' v-for='ay in yil.aylar'>
+                        <tr>
+                            <td>@{{ ay.yil}} - @{{ ay.ay }} </td>
+                            <td class="text-right" v-text='format(ay.miktar)'></td>
+                            <td class="text-right" v-text='format(ay.tutar)'></td>
+                            <td class="text-right" v-text='format(ay.iskonto)'></td>
+                            <td class="text-right" v-text='format(ay.nettutar)'></td>
+                            <td class="text-right" v-text='format(ay.kdv)'></td>
+                            <td class="text-right" v-text='format(ay.toplam)'></td>
+                        </tr>
+                        <div v-if='ay.musteriler.length >0 && yil_index = yil.yil && ay_index = ay.ay' v-for='musteri in ay.musteriler'>
+                            <tr>
+                                <td>@{{ musteri.unvan }} </td>
+                                <td class="text-right" v-text='format(ay.miktar)'></td>
+                                <td class="text-right" v-text='format(ay.tutar)'></td>
+                                <td class="text-right" v-text='format(ay.iskonto)'></td>
+                                <td class="text-right" v-text='format(ay.nettutar)'></td>
+                                <td class="text-right" v-text='format(ay.kdv)'></td>
+                                <td class="text-right" v-text='format(ay.toplam)'></td>
+                            </tr>
+                            <div  v-if='musteri.hesaplar.length >0 && yil_index = yil.yil && ay_index = ay.ay && hesapkod = musteri.hesapkod'>
+                                <tr>
+                                    <td colspan="7">
+                                        <table class="table table-hover table-condenced table-striped text-sm">
+                                            <thead>
+                                                <tr>
+                                                    <th class="text-center">Ürün Kod</th>
+                                                    <th class="text-center">Ürün Ad</th>
+                                                    <th class="text-center">Hesap Kodu</th>
+                                                    <th class="text-center">Evarak Tarihi</th>
+                                                    <th class="text-center">Evrak No</th>
+                                                    <th class="text-center">Ana Miktar</th>
+                                                    <th class="text-center">Birim Fiyat</th>
+                                                    <th class="text-center">Tutar</th>
+                                                    <th class="text-center">İskonto</th>
+                                                    <th class="text-center">Net Tutar</th>
+                                                    <th class="text-center">KDV</th>
+                                                    <th class="text-center">Toplam</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr  v-for='hesap in ay.hesaplar'>
+                                                    <td>@{{ hesap.malkod }}</td>
+                                                    <td>@{{ hesap.malad }}</td>
+                                                    <td>@{{ hesap.hesapkod }}</td>
+                                                    <td>@{{ hesap.evrak_tarih }}</td>
+                                                    <td>@{{ hesap.evrakno }}</td>
+                                                    <td class="text-right" v-text='format(hesap.miktar)'></td>
+                                                    <td class="text-right" v-text='format(hesap.birim_fiyat)'></td>
+                                                    <td class="text-right" v-text='format(hesap.tutar)'></td>
+                                                    <td class="text-right" v-text='format(hesap.iskonto)'></td>
+                                                    <td class="text-right" v-text='format(hesap.net_tutar)'></td>
+                                                    <td class="text-right" v-text='format(hesap.kdv)'></td>
+                                                    <td class="text-right" v-text='format(hesap.toplam)'></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </tbody>
         </table>
     </div>
@@ -47,6 +115,9 @@
         el:'#app',
         data:{
             isLoading:0,
+            yil_index:null,
+            ay_index:null,
+            hesapkod:null,
             detay:null,
             yillik_satislar:{!! $yillik_satislar !!},
             aylik_satislar:null,
@@ -58,7 +129,34 @@
         methods:{
             format(rakam){
                 return  numeral(rakam) . format('0,0.00');
-            }
+            },
+            yilAc(yil){
+                if(this.yil_index == null){
+                    this.yil_index = yil;
+                } else {
+                    this.yil_index = null;
+                    this.ay_index = null;
+                    this.hesapkod = null;
+                    this.hesap_detay = null;
+                }
+            },
+             ayAc(ay){
+                if(this.ay_index == null){
+                    this.ay_index = ay;
+                } else {
+                    this.ay_index = null;
+                    this.hesapkod = null;
+                    this.hesap_detay = null;
+                }
+            },
+            hesapAc(hesapkod){
+                if(this.hesapkod == null){
+                    this.hesapkod = hesapkod;
+                } else {
+                    this.hesapkod = null;
+                    this.hesap_detay = null;
+                }
+            },
         }
     })
 </script>
