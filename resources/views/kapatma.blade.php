c9d4eb5d68a9255a8ad3a2e7a88f1524c032f424
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-11 14:31:01 +0300

    çç

--- resources/views/kapatma.blade.php
+++ resources/views/kapatma.blade.php
@@ -11,13 +11,71 @@
 @section('icerik')
 <div class="row" id="app">
     <div class="col-md-8">
+        <div class="well mb-4">
+            <h4>Kapatma Dışı Borç Hareketleri</h4>
+        </div>
         <div class="well">
-
+            <h4>Kapatma Dışı Alacak Hareketleri</h4>
         </div>
     </div>
     <div class="col-md-4">
-        <div class="well">
-
+        <div class="well mb-4">
+            <h4>Kapatma Dışı Borç Toplam</h4>
+            <div class="list-group">
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Tutar</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Ort. Vade</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Seç Tutar</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Seç Ort. Vade</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="well mb-4">
+            <h4>Kapatma Dışı Alacak Toplam</h4>
+            <div class="list-group">
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Tutar</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Ort. Vade</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Seç Tutar</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-md-4">Seç Ort. Vade</div>
+                        <div class="col-md-8 text-right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
