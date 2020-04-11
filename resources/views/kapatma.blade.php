04816ee13b590ca29695abef685942388aabfb4a
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-11 14:38:10 +0300

    ..

--- resources/views/kapatma.blade.php
+++ resources/views/kapatma.blade.php
@@ -12,67 +12,75 @@
 <div class="row" id="app">
     <div class="col-md-8">
         <div class="card mb-4">
-            <h5>Kapatma Dışı Borç Hareketleri</h5>
+            <div class="card-body">
+            <h5  class="card-title">Kapatma Dışı Borç Hareketleri</h5>
+            </div>
         </div>
         <div class="card">
-            <h5>Kapatma Dışı Alacak Hareketleri</h5>
+            <div class="card-body">
+                <h5  class="card-title">Kapatma Dışı Alacak Hareketleri</h5>
+            </div>
         </div>
     </div>
     <div class="col-md-4">
         <div class="card mb-4">
-            <h5>Kapatma Dışı Borç Toplam</h5>
-            <div class="list-group">
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Tutar</div>
-                        <div class="col-md-8 text-right"></div>
+            <div class="card-body">
+                <h5  class="card-title">Kapatma Dışı Borç Toplam</h5>
+                <div class="list-group">
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Tutar</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Ort. Vade</div>
-                        <div class="col-md-8 text-right"></div>
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Ort. Vade</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Seç Tutar</div>
-                        <div class="col-md-8 text-right"></div>
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Seç Tutar</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Seç Ort. Vade</div>
-                        <div class="col-md-8 text-right"></div>
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Seç Ort. Vade</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="card mb-4">
-            <h5>Kapatma Dışı Alacak Toplam</h5>
-            <div class="list-group">
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Tutar</div>
-                        <div class="col-md-8 text-right"></div>
+            <div class="card-body">
+                <h5  class="card-title">Kapatma Dışı Alacak Toplam</h5>
+                <div class="list-group">
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Tutar</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Ort. Vade</div>
-                        <div class="col-md-8 text-right"></div>
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Ort. Vade</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Seç Tutar</div>
-                        <div class="col-md-8 text-right"></div>
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Seç Tutar</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
-                </div>
-                <div class="list-group-item">
-                    <div class="row">
-                        <div class="col-md-4">Seç Ort. Vade</div>
-                        <div class="col-md-8 text-right"></div>
+                    <div class="list-group-item">
+                        <div class="row">
+                            <div class="col-md-4">Seç Ort. Vade</div>
+                            <div class="col-md-8 text-right"></div>
+                        </div>
                     </div>
                 </div>
             </div>
