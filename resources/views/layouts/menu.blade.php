5d18ef1e15ae7a066460979fd66ed43faedea3b4
Serkan Ciracioglu <sc@Serkans-MacBook-Pro.local>
2020-04-17 03:10:11 +0300

    ..

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -9,9 +9,9 @@
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item "><a class="nav-link" href="/"><i class="fa fa-user-plus fa-fw"></i><span class="text">Yeni Müşteri Seç</span></a></li>
         <li class="nav-item "><a class="nav-link" href="/siparis/create"><i class="fa fa-truck fa-fw"></i><span class="text">Sipariş</span></a></li>
-        <li class="nav-item "><a class="nav-link" href="/siparis_liste"><i class="fa fa-list fa-fw"></i><span class="text">Sipariş Listesi</span></a></li>
-        <li class="nav-item "><a class="nav-link" href="/kapatma"><i class="fa fa-close fa-fw"></i><span class="text">Cari Hesap Kapatma</span></a></li>
-        <li class="nav-item "><a class="nav-link" href="/stok_durum"><i class="fa fa-search fa-fw"></i><span class="text">Depo Stok Durumunuz</span></a></li>
+        <li class="nav-item "><a class="nav-link" href="/siparis_liste"><i class="fa fa-list-ol fa-fw"></i><span class="text">Sipariş Listesi</span></a></li>
+        <li class="nav-item "><a class="nav-link" href="/kapatma"><i class="fa fa-comments-dollar fa-fw"></i><span class="text">Cari Hesap Kapatma</span></a></li>
+        <li class="nav-item "><a class="nav-link" href="/stok_durum"><i class="fa fa-cubes fa-fw"></i><span class="text">Depo Stok Durumunuz</span></a></li>
         <li class="nav-item "><a class="nav-link" href="/rapor"><i class="fa fa-database fa-fw"></i><span class="text">Depo Ürün Stok Durumu</span></a></li>        <hr>
         <li class="nav-item "><a class="nav-link" href="/depo_durum"><i class="fa fa-search fa-fw"></i><span class="text">Genel Stok Durumu</span></a></li>
         @if(session('yetkili') == 1)
